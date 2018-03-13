642f9a466d9865f95bf82591e9c9a332505f6746
Serhii Furtat <32639863+sfurtat@users.noreply.github.com>
2018-03-13 17:55:26 +0200

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './src/ng-zorro-antd.module';
+export { NZ_COLLAPSE_DIRECTIVES as ɵa } from './src/collapse/nz-collapse.module';
+export { DropDownAnimation as ɵbf } from './src/core/animation/dropdown-animations';
+export { FadeAnimation as ɵl } from './src/core/animation/fade-animations';
+export { TagAnimation as ɵbg } from './src/core/animation/tag-animations';
+export { FLOATER_SERVICE_PROVIDER as ɵbm, FLOATER_SERVICE_PROVIDER_FACTORY as ɵbl, FloaterService as ɵbk } from './src/core/floater/floater.service';
+export { FloaterModule as ɵbj } from './src/core/floater/index';
+export { OVERLAY_PROVIDERS as ɵm, OverlayModule as ɵn } from './src/core/overlay/index';
+export { Overlay as ɵo } from './src/core/overlay/overlay';
+export { OVERLAY_CONTAINER_PROVIDER as ɵr, OVERLAY_CONTAINER_PROVIDER_FACTORY as ɵq, OverlayContainer as ɵp } from './src/core/overlay/overlay-container';
+export { ConnectedOverlayDirective as ɵt, OverlayOrigin as ɵs } from './src/core/overlay/overlay-directives';
+export { OverlayPositionBuilder as ɵbd } from './src/core/overlay/position/overlay-position-builder';
+export { VIEWPORT_RULER_PROVIDER as ɵw, VIEWPORT_RULER_PROVIDER_FACTORY as ɵv, ViewportRuler as ɵu } from './src/core/overlay/position/viewport-ruler';
+export { ScrollDispatchModule as ɵx } from './src/core/overlay/scroll/index';
+export { SCROLL_DISPATCHER_PROVIDER as ɵbb, SCROLL_DISPATCHER_PROVIDER_FACTORY as ɵba, ScrollDispatcher as ɵz } from './src/core/overlay/scroll/scroll-dispatcher';
+export { ScrollStrategyOptions as ɵbc } from './src/core/overlay/scroll/scroll-strategy-options';
+export { Scrollable as ɵy } from './src/core/overlay/scroll/scrollable';
+export { NzScrollService as ɵbr, SCROLL_SERVICE_PROVIDER as ɵbt, SCROLL_SERVICE_PROVIDER_FACTORY as ɵbs } from './src/core/scroll/nz-scroll.service';
+export { NzInputNumberComponent as ɵbi } from './src/input-number/nz-input-number.component';
+export { NzSiderComponent as ɵc } from './src/layout/nz-sider.component';
+export { NZ_MESSAGE_DEFAULT_CONFIG as ɵf, NZ_MESSAGE_DEFAULT_CONFIG_PROVIDER as ɵg, NzMessageConfig as ɵe } from './src/message/nz-message-config';
+export { NzMessageBaseService as ɵd } from './src/message/nz-message.service';
+export { NZ_NOTIFICATION_DEFAULT_CONFIG as ɵi, NZ_NOTIFICATION_DEFAULT_CONFIG_PROVIDER as ɵj, NzNotificationConfig as ɵh } from './src/notification/nz-notification-config';
+export { createNzRootInitializer as ɵk } from './src/root/nz-root-config';
+export { NzRootStyleComponent as ɵbq } from './src/root/nz-root-style.component';
+export { NzRootComponent as ɵbp } from './src/root/nz-root.component';
+export { NzOptionPipe as ɵbe } from './src/select/nz-option.pipe';
+export { NzSliderService as ɵbn } from './src/slider/nz-slider.service';
+export { NzStepConnectService as ɵbo } from './src/steps/nz-step-connect.service';
+export { NzTabsInkBarDirective as ɵb } from './src/tabs/nz-tabs-ink-bar.directive';
+export { NzDatePipe as ɵbh } from './src/util/nz-date.pipe';
