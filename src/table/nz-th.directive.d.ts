3aed3a679d4fbc14013e4a7ba052dca156665789
Serhii Furtat <32639863+sfurtat@users.noreply.github.com>
2018-03-14 12:37:32 +0200

    Add files via upload

---
+++ src/table/nz-th.directive.d.ts
@@ -0,0 +1,9 @@
+import { ElementRef } from '@angular/core';
+export declare class NzThDirective {
+    private _elementRef;
+    _el: HTMLElement;
+    nzWidth: any;
+    nzCheckbox: any;
+    nzExpand: any;
+    constructor(_elementRef: ElementRef);
+}
